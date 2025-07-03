33e9288f7bb9f56f6fedbda031ec185b5aa18969
Sumit Rana <sumitrana4224@gmail.com>
2025-07-03 14:21:38 +0530

    Next 1 Question Solved

---
+++ Python/_34.py
@@ -0,0 +1,33 @@
+"""
+"https://www.geeksforgeeks.org/problems/second-largest3735/1?page=1&difficulty=Easy&sortBy=submissions"
+
+Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
+
+Note: The second largest element should not be equal to the largest element.
+
+Examples:
+
+Input: arr[] = [12, 35, 1, 10, 34, 1]
+Output: 34
+Explanation: The largest element of the array is 35 and the second largest element is 34.
+Input: arr[] = [10, 5, 10]
+Output: 5
+Explanation: The largest element of the array is 10 and the second largest element is 5.
+Input: arr[] = [10, 10, 10]
+Output: -1
+Explanation: The largest element of the array is 10 and the second largest element does not exist.
+Constraints:
+2 ≤ arr.size() ≤ 105
+1 ≤ arr[i] ≤ 105
+
+
+
+"""
+
+class Solution:
+    def getSecondLargest(self, arr):
+        arr = list(set(arr))
+        arr.sort()
+        if len(arr) == 1:
+            return -1
+        return arr[-2]
