8fdb2685325430fa033fac52c77abb448c51a36e
Sumit Rana <sumitrana4224@gmail.com>
2025-07-04 14:21:58 +0530

    Next 1 Question Solved

---
+++ Python/_38.py
@@ -0,0 +1,24 @@
+"""
+"https://www.geeksforgeeks.org/problems/sum-of-array2326/1?page=3&difficulty=Basic&sortBy=submissions"
+
+
+You are given an integer array arr[]. The task is to find the sum of it.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 4]
+Output: 10
+Explanation: 1 + 2 + 3 + 4 = 10.
+Input: arr[] = [1, 3, 3]
+Output: 7
+Explanation: 1 + 3 + 3 = 7.
+Constraints:
+1 <= arr.size <= 105
+1 <= arr[i] <= 104
+
+
+"""
+#User function Template for python3
+class Solution:
+	def arraySum(self, arr):
+   		return sum(arr)
