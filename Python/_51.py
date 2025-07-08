872b78e611b17bd0036189350ec63484e422ce6d
Sumit Rana <sumitrana4224@gmail.com>
2025-07-08 14:03:33 +0530

    Next 1 Question Solved

---
+++ Python/_51.py
@@ -0,0 +1,45 @@
+"""
+"https://www.geeksforgeeks.org/problems/triangle-pattern/1?page=6&difficulty=Basic&sortBy=submissions_52.py"
+
+Geek is very fond of patterns. Once, his teacher gave him a pattern to solve. He gave Geek an integer n and asked him to build a pattern.
+
+Help Geek build a star pattern.
+
+Example 1:
+
+Input: 5
+Output:
+* * * * *
+* * * * 
+* * * 
+* *  
+* 
+Example 2:
+
+Input: 3
+Output:
+* * * 
+* *  
+* 
+Your Task:
+You don't need to input anything. Complete the function printTriangle() which takes an integer n  as the input parameter and prints the pattern.
+
+Expected Time Complexity: O(n2).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1<= n <= 100
+
+
+
+"""
+
+#User function Template for python3
+
+class Solution:
+    def printTriangle(self, N):
+        for i in range(N,0,-1):
+            for j in range(i):
+                print("*",end=" ")
+            print("")
+        return
