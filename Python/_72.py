b5a0b433d0c5f813cbbaf674c808a28533c74006
Sumit Rana <sumitrana4224@gmail.com>
2025-07-13 09:33:59 +0530

    Next 1 Question Solved

---
+++ Python/_72.py
@@ -0,0 +1,21 @@
+"""
+"https://www.geeksforgeeks.org/problems/start-coding/1?page=6&difficulty=Basic&sortBy=submissions"
+
+When learning a new language, we first learn to output some message. Here, we'll start with the famous Hello World message. Now, here you are given a function to complete. Don't worry about the input and output of functions, just print "Hello World" message 
+
+Example 1
+
+Input: No input
+Output: Hello World
+Your Task:
+Your task is to complete the function printHello() that prints Hello World.
+
+Expected Time Complexity: O(1)
+Expected Auxilary Space : O(1)
+
+
+"""
+class Solution:
+    def printHello(self) -> None:
+        print("Hello World")
+        
