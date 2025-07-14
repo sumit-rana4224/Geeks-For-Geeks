88b6df166c0fa61b262b8ee1f657240fb2ab7b53
Sumit Rana <sumitrana4224@gmail.com>
2025-07-14 14:25:29 +0530

    Next 1 question solved

---
+++ Python/_76.py
@@ -0,0 +1,18 @@
+"""
+"https://www.geeksforgeeks.org/problems/input-integer/1?page=9&difficulty=Basic&sortBy=submissions"
+Given a number n. Take input and add 10 to its value and print it.
+
+Examples:
+
+Input: n = 2
+Output: 12
+Explanation: Input is 2 adding 10 to it makes it 12.
+Input: n = 22
+Output: 32
+Explanation: Input is 22 adding 10 to it makes it 32.
+
+"""
+# Take input
+# Add 10 and Print
+x = int(input())
+print(x+10)
