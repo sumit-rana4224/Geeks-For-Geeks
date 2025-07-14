2e410dd0a5efb7aa804a78a226f87c90266436bd
Sumit Rana <sumitrana4224@gmail.com>
2025-07-14 14:25:23 +0530

    Next 1 question solved

---
+++ Python/_75.py
@@ -0,0 +1,20 @@
+"""
+"https://www.geeksforgeeks.org/problems/length-of-string/1?page=13&difficulty=Basic&sortBy=submissions"
+
+You are given a string s. You need to find the length of the string and return it.
+
+Examples:
+
+Input: s = Geeks
+Output: 5
+Input: s = Hello1234
+Output: 9
+Constraints:
+1 <= | s |  <= 100000
+"""
+#User function Template for python3
+
+class Solution:
+    def lengthString(self, str):
+        return len(str)
+    
