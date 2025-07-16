f54ff536eec3b159870e402041e5de0bd57a964f
Sumit Rana <sumitrana4224@gmail.com>
2025-07-16 23:45:49 +0530

    Next 1 question solved

---
+++ Python/_85.py
@@ -0,0 +1,15 @@
+"""
+Given two integers a and b, you need to concatenate them so the output is ab and print it.
+
+Examples:
+
+Input: a = 5, b = 6
+Output: 56
+Explanation: Concatenate them.
+Input: a = 1, b = 9
+Output: 19
+Explanation: Concatenate them.
+"""
+a = int(input())
+b = int(input())
+print(f"{a}{b}")
