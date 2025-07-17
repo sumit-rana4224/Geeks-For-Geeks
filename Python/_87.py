3ad53a9704e3a124361caf26c171512583ca7dfd
Sumit Rana <sumitrana4224@gmail.com>
2025-07-17 18:49:18 +0530

    Next 1 question solved

---
+++ Python/_87.py
@@ -0,0 +1,18 @@
+"""
+Given two inputs that are stored in variables a and b, you need to print a and b in a single line without space betweenthem.
+Note: Print a new line at the end after printing.
+
+Examples:
+
+Input:a = "Hello", b = "World"
+Output: HelloWorld
+Explanation: a and b are printed in a single line without space between them.
+"""
+#User function Template for python3
+
+
+def utility():
+    a = input()
+    b = input()
+    #Write your code below that prints ab
+    print(f"{a}{b}")
