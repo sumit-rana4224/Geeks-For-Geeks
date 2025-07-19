e6b3e34a028fdcb24dc9d17ed33914c7244f9a3f
Sumit Rana <sumitrana4224@gmail.com>
2025-07-19 21:34:36 +0530

    Next 1 Question Solved

---
+++ Python/_97.py
@@ -0,0 +1,18 @@
+"""
+Given a string s, you need to reverse it.
+
+Examples
+
+Input:s = "Hello"
+Output:olleH
+Explanation: Reverse of Hello is olleH
+Input:s = "World"
+Output:dlroW
+Explanation: Reverse of World is dlroW
+Your Task: 
+The input s is provided as a parameter to the function reverseString(). You need to reverse it and return it so it can be printed by the driver code.
+"""
+#User function Template for python3
+
+def reverseString(s):
+    return s[::-1]
