cc51f7096b4a23f26bf48548c3a2c39d2417ce2c
Sumit Rana <sumitrana4224@gmail.com>
2025-07-20 23:05:21 +0530

    Next 1 Question Solved

---
+++ Python/_101.py
@@ -0,0 +1,22 @@
+"""
+Given an input num as a string. You need to typecast into an integer and double it. 
+
+Examples:
+
+Input: num = "5"
+Output: 10
+Explanation: Typecast "5" to int and then double it 5 * 2 = 10
+Input: num = "12"
+Output: 24
+Explanation: Typecast "12" to int and then double it 12 * 2 = 24
+Constraints:
+0 <= num <= 105
+
+"""
+num = input()
+
+########### Write your code below ###############
+# TypeCast num to int, double it and print
+
+########### Write your code above ###############
+print(int(num)*2)
