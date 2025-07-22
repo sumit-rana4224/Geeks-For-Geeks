4385cf62af317c583e66eef27ca717d112e84748
Sumit Rana <sumitrana4224@gmail.com>
2025-07-22 17:11:53 +0530

    Next 1 Question Solved

---
+++ Python/_109.py
@@ -0,0 +1,20 @@
+"""
+You'll be given two strings a and b, a separator symbol, and you need to print a and b such that a and b are separated by the separator symbol followed by a new line.
+
+Examples:
+
+Input: a = "Hello", b = "World", separator = "@"
+Output: Hello@World
+Explanation: a and b are printed with the separator symbol in between.
+
+"""
+#User function Template for python3
+
+
+def utility():
+    a = input()
+    b = input()
+    separator = input()
+
+    #Your code below. Use sep=
+    print(a+separator+b)
