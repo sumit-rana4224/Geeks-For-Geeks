b1c39f22d8c689a3678a2aabe1bcf97fdf9a5b17
Sumit Rana <sumitrana4224@gmail.com>
2025-07-25 20:48:11 +0530

    Next 1 Question Solved

---
+++ Python/_120.py
@@ -0,0 +1,12 @@
+"""
+Given a text in the form of a string, you need to take input and print it.
+
+Examples:
+
+Input: s = "abcd"
+Output: "abcd"
+Explanation: The input is printed as it is.
+
+"""
+# Take String Input and Print it
+print(input())
