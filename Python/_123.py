2e08c1ad31f49bb39a1863e0ed15047fbc6bc56e
Sumit Rana <sumitrana4224@gmail.com>
2025-07-26 14:37:39 +0530

    Next 1 Question Solved

---
+++ Python/_123.py
@@ -0,0 +1,31 @@
+"""
+You are given a square matrix of size n x n. Your task is to find the transpose of the given matrix.
+The transpose of a matrix is obtained by converting all the rows to columns and all the columns to rows.
+
+Examples :
+
+Input: mat[][] = [[1, 1, 1, 1],
+                [2, 2, 2, 2],
+                [3, 3, 3, 3],
+                [4, 4, 4, 4]]
+Output: [[1, 2, 3, 4],
+       [1, 2, 3, 4],
+       [1, 2, 3, 4],
+       [1, 2, 3, 4]]
+Explanation: Converting rows into columns and columns into rows.
+Input: mat[][] =  [[1, 2],
+                 [9, -2]]
+Output: [[1, 9],
+        [2, -2]]
+Explanation: Converting rows into columns and columns into rows.
+Constraints:
+1 ≤ n ≤ 103
+-109 ≤ mat[i][j] ≤109
+"""
+import numpy as np
+class Solution:
+    def transpose(self, mat):
+        arr = np.array(mat)
+        x = arr.T
+        return x
+        
