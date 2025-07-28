6a1db7a63264f9be304c4124588d9d088a7e837e
Sumit Rana <sumitrana4224@gmail.com>
2025-07-28 10:18:16 +0530

    Next 1 Question Solved

---
+++ Python/_132.py
@@ -0,0 +1,17 @@
+"""
+Given a natural number n. You have to find the number of digits in it and return it.
+
+Examples:
+
+Input: n = 12
+Output: 2
+Explanation: 12 has 2 digits
+Input: n = 456
+Output: 3
+Explanation: 456 has 3 digits
+Constraints:
+1 ≤ n ≤ 105
+"""
+class Solution:
+    def countDigits(self, n):
+        return len(str(n))
