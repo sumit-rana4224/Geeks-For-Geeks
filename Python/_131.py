b98f5d416f103a0f23b1e419c405f49eb8fbd1ca
Sumit Rana <sumitrana4224@gmail.com>
2025-07-28 10:18:11 +0530

    Next 1 Question Solved

---
+++ Python/_131.py
@@ -0,0 +1,16 @@
+"""
+Given a number n, find the first digit of the number.
+
+Examples:
+
+Input: n = 123
+Output: 1
+Input: n = 976
+Output: 9
+Constraints:
+1 <= n <= 109
+
+
+"""
+def firstDigit(n):
+    return str(n)[0]
