c6402be9be76317af46c58f142e1acfe1ee7241c
Sumit Rana <sumitrana4224@gmail.com>
2025-07-28 10:18:21 +0530

    Next 1 Question Solved

---
+++ Python/_133.py
@@ -0,0 +1,15 @@
+"""
+Given a double value d, typecast it to an integer value and return it.
+
+Example:
+
+Input: d = 10.23
+Output: 10
+Explanation: The integer value of 10.23 is 10
+
+"""
+#User function Template for python3
+def typeCast(d):
+    # Your code here
+    # Convert double d to int and return
+    return int(d)
