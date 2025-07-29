2ba24b2fabaf9753203b7c8283e8409e76877e5a
Sumit Rana <sumitrana4224@gmail.com>
2025-07-29 22:15:50 +0530

    Next 1 Question Solved

---
+++ Python/_135.py
@@ -0,0 +1,37 @@
+"""
+You are given two variables, a and b. Your task is to print these variables in the following formats:
+
+With space: Print a and b in a single line, separated by a space, followed by a newline.
+Without newline: Print a and b separated by a space, but do not end the output with a newline.
+With separator: Print a and b separated by a specified separator, followed by a newline.
+Without space: Print a and b in a single line, with no spaces between them, followed by a newline.
+Examples:
+
+Input: a = "Hello", b = "World", separator = '&'
+Output: 
+Hello World
+Hello WorldHello&World
+HelloWorld
+Explanation: 
+With space: a and b are printed in a single line with a space separating them. (Hello World)
+Without newline: a and b are printed in a single line with a space separating them, but no newline is added after. (Hello World with no newline).
+With separator: a and b are printed in a single line, separated by the given separator character. (Hello&World)
+Without space: a and b are printed directly next to each other with no space between them. (HelloWorld)
+
+"""
+#User function Template for python3
+a = input()
+b = input()
+separator = input()[0]
+
+########### Write your code below ###############
+
+# Print with space
+print(a,b)
+# Print without newline at the end
+print(a,b,end="")
+# Print with separator
+print(a+separator+b)
+# Print without space
+print(a+b)
+########### Write your code above ###############
