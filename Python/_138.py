c95d45db74e6b6ae34f44e5b694627f6056d5fb3
Sumit Rana <sumitrana4224@gmail.com>
2025-07-30 11:48:12 +0530

    Next 1 Question Solved

---
+++ Python/_138.py
@@ -0,0 +1,13 @@
+"""
+Comment the code that outputs "Hello World", so that "GeeksForGeeks" gets printed.
+
+Example: 
+
+Output: "GeeksForGeeks"
+"""
+
+#User function Template for python3
+# Prints Hello World
+# print("Hello World")
+# Prints GeeksForGeeks
+print("GeeksForGeeks")
